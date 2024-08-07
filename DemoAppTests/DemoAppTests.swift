8503ee572e07981dc4ab4d7c29bac6fc4a77b039
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2024-08-07 11:54:26 +0500

    Initial Commit

---
+++ DemoAppTests/DemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppTests.swift
+//  DemoAppTests
+//
+//  Created by Zohaib Ahmed on 8/7/24.
+//
+
+import Testing
+@testable import DemoApp
+
+struct DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
