2274450bbf4be603f5f238213c959b42757d0bac
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2024-08-07 21:57:05 +0500

    Filter button implementaiton

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -8,17 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var loanStore = LoanStore()
+    
+    @State private var filterEnabled = false
+    @State private var maximuimLoanAmount = 10000.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack{
+                
+                if filterEnabled {
+                    LoanFilterView(amount: self.$maximuimLoanAmount)
+                        .transition(.opacity)
+                }
+                
+                List(loanStore.loans) { loan in
+                    LoanCellView(loan: loan)
+                        .padding(.vertical, 5)
+                }.listStyle(.plain)
+            }
+            
+            .navigationTitle("Kiva Loan")
+            .toolbar{
+                ToolbarItem(placement:.topBarTrailing){
+                    Button {
+                        withAnimation(.linear){
+                            self.filterEnabled.toggle()
+                            self.loanStore.filterLoans(maxAmount: Int(self.maximuimLoanAmount))
+                        }
+                    } label: {
+                        Text("Filter")
+                            .font(.subheadline)
+                            .foregroundStyle(.primary)
+                    }
+                }
+            }
         }
-        .padding()
+        .task {
+            self.loanStore.fetchLatestLoans()
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+#Preview {
+    LoanFilterView(amount: .constant(10.0))
+}
+
+struct LoanFilterView: View{
+    
+    @Binding var amount: Double
+    
+    var minAmount = 0.0
+    var maxAmount = 10000.0
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            Text("Show loan amount below $\(Int(amount))")
+                .font(.system(.headline, design: .rounded))
+            
+            HStack{
+                Slider(value: $amount, in: minAmount...maxAmount, step: 100)
+                    .tint(.purple)
+            }
+            HStack{
+                Text("\(Int(minAmount))")
+                    .font(.system(.footnote, design: .rounded))
+                Spacer()
+                Text("\(Int(maxAmount))")
+                    .font(.system(.footnote, design: .rounded))
+            }
+        }
+        .padding(.horizontal)
+        .padding(.bottom, 10)
+    }
+}
+
+struct LoanCellView: View {
+    var loan: Loan
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Text(loan.name)
+                    .font(.system(.headline, design: .rounded))
+                    .bold()
+                Text(loan.country)
+                    .font(.system(.subheadline, design: .rounded))
+                Text(loan.use)
+                    .font(.system(.body, design: .rounded))
+            }
+            Spacer()
+            VStack {
+                Text("$\(loan.amount)")
+                    .font(.system(.title, design: .rounded))
+                    .bold()
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+    }
+}
+
--- DemoApp/Model/Loan.swift
+++ DemoApp/Model/Loan.swift
@@ -6,3 +6,101 @@
 //
 
 import Foundation
+
+struct Loan: Identifiable{
+    var id = UUID()
+    var name: String
+    var country: String
+    var use: String
+    var amount: Int
+    
+    init(name: String, country: String, use: String, amount: Int) {
+        self.name = name
+        self.country = country
+        self.use = use
+        self.amount = amount
+    }
+}
+
+extension Loan: Codable{
+    enum CodingKeys: String, CodingKey{
+        case name
+        case country = "location"
+        case use
+        case amount = "loan_amount"
+    }
+    
+    enum LocationKeys: String, CodingKey{
+        case country
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        let location = try container.nestedContainer(keyedBy: LocationKeys.self, forKey: .country)
+        country = try location.decode(String.self, forKey: .country)
+        use = try container.decode(String.self, forKey: .use)
+        amount = try container.decode(Int.self, forKey: .amount)
+    }
+}
+
+class LoanStore: Decodable, ObservableObject{
+    @Published var loans: [Loan] = []
+    private var cachedLoans: [Loan] = []
+    
+    enum CodingKeys: CodingKey{
+        case loans
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        loans = try values.decode([Loan].self, forKey: .loans)
+    }
+    
+    init(){
+        
+    }
+    
+    private static var kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
+    
+    
+    func fetchLatestLoans(){
+        guard let loanUrl = URL(string: Self.kivaLoanURL) else {
+            return
+        }
+        
+        let request = URLRequest(url: loanUrl)
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
+            
+            if let error {
+                print(error)
+                return
+            }
+            
+            if let data{
+                DispatchQueue.main.async{
+                    self.loans = self.parseJsonData(data: data)
+                    self.cachedLoans = self.loans
+                }
+            }
+        })
+        
+        task.resume()
+    }
+    func filterLoans(maxAmount: Int) {
+        self.loans = self.cachedLoans.filter{ $0.amount < maxAmount}
+    }
+    
+    func parseJsonData(data: Data) -> [Loan]{
+        let decoder = JSONDecoder()
+        
+        do{
+            let loanStore = try decoder.decode(LoanStore.self, from: data)
+            self.loans = loanStore.loans
+        } catch {
+            print(error)
+        }
+        
+        return loans
+    }
+}
